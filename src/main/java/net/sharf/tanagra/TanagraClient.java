700fe775f2a9b8561494fa28e8f10998df2600ce
Sharf <aellen.virwolt@yandex.ru>
2023-02-12 21:53:08 +0100

    Initial commit

---
+++ src/main/java/net/sharf/tanagra/TanagraClient.java
@@ -0,0 +1,10 @@
+package net.sharf.tanagra;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TanagraClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
